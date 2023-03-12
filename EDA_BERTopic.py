8c881ec3415fc310c3ebd53d828c026739bb1c2e
AndresSanders <andres.sanders@hotmail.com>
2023-03-12 19:14:02 +0100

    Added plotting functionality

--- EDA_BERTopic.py
+++ EDA_BERTopic.py
@@ -2,6 +2,7 @@ from bertopic import BERTopic
 import csv
 import functions
 import pandas as pd
+import plotly
 
 csvFilepath = "Data/TwitterExport/data.csv"
 rows = functions.read_csv_file(csvFilepath)
@@ -12,6 +13,7 @@ conversations = conversations[:1000]
 
 topic_model = BERTopic(embedding_model="all-MiniLM-L6-v2")
 topics, probs = topic_model.fit_transform(conversations)
-topic_model.
 print(topic_model.get_topic_info())
-print(topic_model.)
+fig = topic_model.visualize_topics()
+plotly.offline.plot(fig, filename='topic_model.html')
+
